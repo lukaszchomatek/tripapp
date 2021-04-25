3cc66a13e4c8a00d296baa283956cdc191db0570
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2021-04-25 16:05:54 +0200

    added: - marking comments as not proper - GET/comments shows only proper comments

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -5,6 +5,7 @@ exports.getCommentsForPlace = async (req, res) => {
     const place = req.body.place;
     try {
         let commentsForPlace = await Comment.find({
+            proper : true,
             place : place._id
         })
         res.json(commentsForPlace)
@@ -36,7 +37,33 @@ exports.createCommentForPlace = async (req, res) => {
     }
 }
 
-exports.markCommentNotProper = (req, res) => {
-    res.sendStatus(500);
+exports.markCommentNotProper = async (req, res) => {
+    // znajdujemy comment o podanym id w bazie
+    let comment;
+    try {
+        //console.log(req.params.id)
+        comment = await Comment.findById(req.params.id);
+        //console.log(comment)
+        if (comment == null) {
+            return res.status(404).json({ 
+                message: "Comment not found"
+            })
+        }
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
+    }
+    
+    // oznaczamy go jako nieodpowiedni i zapisujemy zmianę w bazie
+    comment.proper = false;
+    try {
+        let markedComment = await comment.save()
+        res.sendStatus(204);
+    } catch (err) {
+        res.status(400).json({
+            message : err.message
+        })
+    }
 }
 
