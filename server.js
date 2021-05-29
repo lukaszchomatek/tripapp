243d3b76c35f11e21463ae21807a7b5ca3a71d5c
lukaszchomatek <lukasz.chomatek@p.lodz.pl>
2021-05-29 15:05:55 +0200

    configured for jest

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
-const db = mongoose.connection
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to DB'))
 
 const router = require('./routes/index')
 app.use(express.json())
 app.use('/', router)
 
-app.listen(3000, () => {
-    console.log("TripApp started")
-})
+module.exports = app;
